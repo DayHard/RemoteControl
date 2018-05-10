fabaef9b44e01e5baceb7d3ddc0c59d081c1395d
DayHard <vladimir.landenok@gmail.com>
2018-05-10 14:32:27 +0300

    Version 0.1.6704.25088 from 10.05.2018 14 31 - Частичная реализация интерфейса

--- RemoteControl/MainWindow.xaml.cs
+++ RemoteControl/MainWindow.xaml.cs
@@ -1,28 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO.Ports;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace RemoteControl
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
+        private SerialPort _port;
+        private bool _portIsOpen;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Grid_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Считываем версию ПО
+            LbVersion.Content = "Версия: " + Assembly.GetExecutingAssembly().GetName().Version;
+            // Считываем доступные COM порты
+            foreach (var portName in SerialPort.GetPortNames())
+            {
+                CbPortName.Items.Add(portName);
+            }
+        }
+        //Подключение по COM
+        private void BtnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            if (!_portIsOpen)
+            {
+                _port = new SerialPort();
+
+                try
+                {
+                    _port.PortName = Convert.ToString(CbPortName.SelectionBoxItem);
+                    _port.BaudRate = Convert.ToInt32(CbBaudRate.SelectionBoxItem);
+                    _port.StopBits = StopBits.One;
+                    _port.DataBits = 8;
+                    _port.StopBits = StopBits.One;
+                    _port.Parity = Parity.Even;
+                    _port.Open();
+
+                    BtnConnect.Content = "Отключить";
+                    LbConnectionStatus.Content = "Статус: Подключено";
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(this, ex.Message, "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            }
+            else
+            {
+                _port.Close();
+                BtnConnect.Content = "Подключить";
+                LbConnectionStatus.Content = "Статус: Отключено";
+            }
+
+            _portIsOpen = !_portIsOpen;
+            CbPortName.IsEnabled = !CbPortName.IsEnabled;
+            CbBaudRate.IsEnabled = !CbBaudRate.IsEnabled;
         }
     }
 }
