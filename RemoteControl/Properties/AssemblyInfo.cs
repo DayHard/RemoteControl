4d6b366bd37154cf617f88a1741f1418723b8873
DayHard <vladimir.landenok@gmail.com>
2018-05-24 15:09:07 +0300

    Version 0.9.6718.27246 from 24.05.2018 15 09

--- RemoteControl/Properties/AssemblyInfo.cs
+++ RemoteControl/Properties/AssemblyInfo.cs
@@ -49,5 +49,5 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.8.*")]
+[assembly: AssemblyVersion("0.9.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
